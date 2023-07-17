3032691b902567a3fb11b7f0ec33ad613be5a02e
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-07-17 17:20:28 -0300

    feat: dark and light mode

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 const span = document.querySelector("span");
-const play = document.querySelector(".play");
-const stop = document.querySelector(".stop");
-const addFive = document.querySelector(".add");
-const decrementFive = document.querySelector(".decrease");
+const play = document.querySelector(".play svg path");
+const stop = document.querySelector(".stop svg path");
+const addFive = document.querySelector(".add svg path");
+const decrementFive = document.querySelector(".decrease svg path");
+const body = document.querySelector("body");
+const sun = document.querySelectorAll("header img")[0];
+const moon = document.querySelectorAll("header img")[1];
+const florestInput = document.querySelector(".florest input");
+const rainInput = document.querySelector(".rain input");
+const fireInput = document.querySelector(".fire input");
+const standInput = document.querySelector(".stand input");
 
 let minutesDisplay = parseInt(span.innerHTML.slice(0, 2));
 let secondsDisplay = parseInt(span.innerHTML.slice(3, 5));
@@ -90,25 +97,59 @@ function setAudio(audio) {
   }
 }
 
-function resetSndCards(svg) {
-  const paths = svg.querySelectorAll("path");
+florest.addEventListener("input", () => {
+  sound1.volume = florestInput.value / 100;
+});
+
+stand.addEventListener("input", () => {
+  sound2.volume = standInput.value / 100;
+});
+
+rain.addEventListener("input", () => {
+  sound3.volume = rainInput.value / 100;
+});
+
+fire.addEventListener("input", () => {
+  sound4.volume = fireInput.value / 100;
+});
+
+function resetSndCards(button) {
+  const paths = button.querySelectorAll("path");
+  const input = button.querySelector("input");
   currentColor = !currentColor;
-  if (currentColor) {
-    if (paths.length >= 2) {
-      const secondPath = paths[1];
-      secondPath.style.fill = "#fff";
+  if (!body.classList.contains("dark")) {
+    if (currentColor) {
+      paths[0].style.fill = "#fff";
+      button.style.backgroundColor = "#02799D";
+      input.classList.add("active");
     } else {
-      console.log("Não foi encontrado o segundo path no SVG.");
+      paths[0].style.fill = "#323238";
+      button.style.backgroundColor = "#E1e1e6";
+      input.classList.remove("active");
     }
-    paths[0].style.fill = "#02799D";
   } else {
-    if (paths.length >= 2) {
-      const secondPath = paths[1];
-      secondPath.style.fill = "#323238";
+    if (currentColor) {
+      paths[0].style.fill = "#fff";
+      button.style.backgroundColor = "#02799D";
+      input.classList.add("active");
     } else {
-      console.log("Não foi encontrado o segundo path no SVG.");
+      paths[0].style.fill = "#E1e1e6";
+      button.style.backgroundColor = "#323238";
+      input.classList.remove("active");
     }
-    paths[0].style.fill = "#E1E1E6";
+  }
+}
+
+function restoreSndCardsColors(svg) {
+  const paths = svg.querySelectorAll("path");
+  if (!body.classList.contains("dark")) {
+    // Restaurar a cor do primeiro path
+    paths[0].style.fill = "#323238";
+    svg.style.backgroundColor = "#e1e1e6";
+
+  }else{
+    paths[0].style.fill = "#e1e1e6";
+    svg.style.backgroundColor = "#323238";
   }
 }
 
@@ -130,4 +171,24 @@ rain.addEventListener("click", () => {
 fire.addEventListener("click", () => {
   setAudio(sound4);
   resetSndCards(fire);
+});
+
+sun.addEventListener("click", () => {
+  sun.classList.add("hide");
+  moon.classList.remove("hide");
+  body.classList.toggle("dark");
+  restoreSndCardsColors(florest);
+  restoreSndCardsColors(rain);
+  restoreSndCardsColors(stand);
+  restoreSndCardsColors(fire);
+});
+
+moon.addEventListener("click", () => {
+  moon.classList.add("hide");
+  sun.classList.remove("hide");
+  body.classList.toggle("dark");
+  restoreSndCardsColors(florest);
+  restoreSndCardsColors(rain);
+  restoreSndCardsColors(stand);
+  restoreSndCardsColors(fire);
 });
